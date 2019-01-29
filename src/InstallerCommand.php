2b2f4822b3d6f915c9274032fac106d8fd86bd0b
phuc tran <phuc.thanh.tran@seldatinc.com>
2019-01-29 16:20:41 +0700

    init

---
+++ src/InstallerCommand.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 1/29/2019
+ * Time: 3:51 PM
+ */
+
+namespace Saas\Installer;
+
+use Symfony\Component\Console\Command\Command;
+
+class InstallerCommand extends Command
+{
+
+    /**
+     * InstallerCommand constructor.
+     */
+    public function __construct()
+    {
+        echo "Ali33 kaka SAAS";
+    }
+}
