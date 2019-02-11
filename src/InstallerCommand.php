7f6266decb1454dd031cbaf6a2218bbed9ce2004
phuc tran <phuc.thanh.tran@seldatinc.com>
2019-02-11 08:54:04 +0700

    update

--- src/InstallerCommand.php
+++ src/InstallerCommand.php
@@ -8,16 +8,109 @@
 
 namespace Saas\Installer;
 
+use GuzzleHttp\Client;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use ZipArchive;
+use Symfony\Component\Filesystem\Filesystem;
 
 class InstallerCommand extends Command
 {
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('install')
+            ->setDescription('Create a new Saas Product Application')
+            ->addArgument('name', InputArgument::OPTIONAL);
+    }
 
     /**
-     * InstallerCommand constructor.
+     * Download the temporary Zip to the given file.
+     *
+     * @param  string  $zipFile
+     * @param  string  $version
+     * @return $this
      */
-    public function __construct()
+    protected function download($zipFile, $version = 'master')
     {
-        echo "Ali33 kaka SAAS";
+        switch ($version) {
+            case 'develop':
+                $filename = 'latest-develop.zip';
+                break;
+            case 'master':
+                $filename = 'latest.zip';
+                break;
+        }
+        $response = (new Client)->get('http://cabinet.laravel.com/'.$filename);
+        file_put_contents($zipFile, $response->getBody());
+        return $this;
+    }
+
+    /**
+     * Extract the Zip file into the given directory.
+     *
+     * @param  string  $zipFile
+     * @param  string  $directory
+     * @return $this
+     */
+    protected function extract($zipFile, $directory)
+    {
+        $archive = new ZipArchive;
+        $archive->open($zipFile);
+        $archive->extractTo($directory);
+        $archive->close();
+        return $this;
+    }
+
+    /**
+     * Clean-up the Zip file.
+     *
+     * @param  string  $zipFile
+     * @return $this
+     */
+    protected function cleanUp($zipFile)
+    {
+        @chmod($zipFile, 0777);
+        @unlink($zipFile);
+        return $this;
+    }
+
+    /**
+     * Make sure the storage and bootstrap cache directories are writable.
+     *
+     * @param  string  $appDirectory
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return $this
+     */
+    protected function prepareWritableDirectories($appDirectory, OutputInterface $output)
+    {
+        $filesystem = new Filesystem;
+        try {
+            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR."bootstrap/cache", 0755, 0000, true);
+            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR."storage", 0755, 0000, true);
+        } catch (IOExceptionInterface $e) {
+            $output->writeln('<comment>You should verify that the "storage" and "bootstrap/cache" directories are writable.</comment>');
+        }
+        return $this;
+    }
+
+    /**
+     * Get the composer command for the environment.
+     *
+     * @return string
+     */
+    protected function findComposer()
+    {
+        if (file_exists(getcwd().'/composer.phar')) {
+            return '"'.PHP_BINARY.'" composer.phar';
+        }
+        return 'composer';
     }
 }
