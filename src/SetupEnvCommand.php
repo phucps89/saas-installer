166ff11bfdef68b10779e6d2d9a7d9263251a9b7
phuc tran <phuc.thanh.tran@seldatinc.com>
2019-02-13 09:08:33 +0700

    update

--- src/SetupEnvCommand.php
+++ src/SetupEnvCommand.php
@@ -23,6 +23,18 @@ class SetupEnvCommand extends Command
     const ZEPHIR_PARSER = 'Zephir Parser';
     const GIT_REPO_ZEPHIR_PARSER = 'https://github.com/phalcon/php-zephir-parser.git';
 
+    private $os;
+
+    /**
+     * @var InputInterface
+     */
+    private $input;
+
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+
     /**
      * Configure the command options.
      *
@@ -37,6 +49,9 @@ class SetupEnvCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->os = Common::operating_system_detection()['id'];
+        $this->input = $input;
+        $this->output = $output;
         $helper = $this->getHelper('question');
         $arrEnv = [
             1 => self::ZEPHIR_PARSER,
@@ -47,22 +62,18 @@ class SetupEnvCommand extends Command
 
         switch ($ans){
             case self::ZEPHIR_PARSER:
-                $this->installZephirParser($input, $output);
+                $this->installZephirParser();
                 break;
         }
     }
 
-    private function installZephirParser(InputInterface $input, OutputInterface $output)
+    private function installZephirParser()
     {
-        $io = new SymfonyStyle($input, $output);
+        $io = new SymfonyStyle($this->input, $this->output);
         if(!Common::isRunningAsRoot()){
             $io->caution("This action must be run as Root permission!!");
             return;
         }
-
-        $phpVersion = Common::getPhpVersion();
-        system('apt-get update');
-        system("apt-get install php{$phpVersion}-dev gcc make re2c autoconf automake");
 
         $dirZephir = '/php-zephir-parser';
         if(file_exists($dirZephir)){
@@ -71,6 +82,17 @@ class SetupEnvCommand extends Command
 
         $io->note("Cloning Zephir Parser...");
         $repo = GitRepository::cloneRepository('https://github.com/phalcon/php-zephir-parser.git', $dirZephir);
+
+
+        $io->note("Installing dependencies...");
+        $phpVersion = Common::getPhpVersion();
+        if($this->os == 'ubuntu'){
+            system("apt-get install php{$phpVersion}-dev gcc make re2c autoconf automake");
+        }
+        else if (in_array($this->os, ['centos', 'fedora'])){
+            system("yum install php-devel gcc make re2c autoconf automake");
+        }
+
 
         $io->note("Installing Zephir Parser...");
         $logCurDir = getcwd();
@@ -82,6 +104,8 @@ class SetupEnvCommand extends Command
 
         chdir($logCurDir);
         Common::deleteDirectory($dirZephir);
+        $io->note("Add the extension to your php.ini!!");
+        $io->section("[Zephir Parser]\nextension=zephir_parser.so");exit;
         $io->note("Remember to restart Web server!!");
     }
 
--- src/Utils/Common.php
+++ src/Utils/Common.php
@@ -42,4 +42,93 @@ class Common
         $version = explode('.', PHP_VERSION);
         return $version[0].'.'.$version[1];
     }
+
+    public static function operating_system_detection(){
+        if (false == function_exists("shell_exec") || false == is_readable("/etc/os-release")) {
+            return null;
+        }
+
+        $os         = shell_exec('cat /etc/os-release');
+        $listIds    = preg_match_all('/.*=/', $os, $matchListIds);
+        $listIds    = $matchListIds[0];
+
+        $listVal    = preg_match_all('/=.*/', $os, $matchListVal);
+        $listVal    = $matchListVal[0];
+
+        array_walk($listIds, function(&$v, $k){
+            $v = strtolower(str_replace('=', '', $v));
+        });
+
+        array_walk($listVal, function(&$v, $k){
+            $v = preg_replace('/=|"/', '', $v);
+        });
+
+        return array_combine($listIds, $listVal);
+    }
+
+    /**
+     * Get the composer command for the environment.
+     *
+     * @return string
+     */
+    public static function findComposer()
+    {
+        if (file_exists(getcwd().'/composer.phar')) {
+            return '"'.PHP_BINARY.'" composer.phar';
+        }
+        return 'composer';
+    }
+
+    public static  function setEnvironmentValue($envFile, array $values)
+    {
+        $str = file_get_contents($envFile);
+
+        if (count($values) > 0) {
+            foreach ($values as $envKey => $envValue) {
+
+                $str .= "\n"; // In case the searched variable is in the last line without \n
+                $keyPosition = strpos($str, "{$envKey}=");
+                $endOfLinePosition = strpos($str, "\n", $keyPosition);
+                $oldLine = substr($str, $keyPosition, $endOfLinePosition - $keyPosition);
+
+                // If key does not exist, add it
+                if (!$keyPosition || !$endOfLinePosition || !$oldLine) {
+                    $str .= "{$envKey}={$envValue}\n";
+                } else {
+                    $str = str_replace($oldLine, "{$envKey}={$envValue}", $str);
+                }
+
+            }
+        }
+
+        $str = substr($str, 0, -1);
+        if (!file_put_contents($envFile, $str)) return false;
+        return true;
+    }
+
+    public static function isAbsolutePath($path) {
+        if (!is_string($path)) {
+            $mess = sprintf('String expected but was given %s', gettype($path));
+            throw new \InvalidArgumentException($mess);
+        }
+        if (!ctype_print($path)) {
+            $mess = 'Path can NOT have non-printable characters or be empty';
+            throw new \DomainException($mess);
+        }
+        // Optional wrapper(s).
+        $regExp = '%^(?<wrappers>(?:[[:print:]]{2,}://)*)';
+        // Optional root prefix.
+        $regExp .= '(?<root>(?:[[:alpha:]]:/|/)?)';
+        // Actual path.
+        $regExp .= '(?<path>(?:[[:print:]]*))$%';
+        $parts = [];
+        if (!preg_match($regExp, $path, $parts)) {
+            $mess = sprintf('Path is NOT valid, was given %s', $path);
+            throw new \DomainException($mess);
+        }
+        if ('' !== $parts['root']) {
+            return true;
+        }
+        return false;
+    }
 }
